b9a9e422cf76df73fb90b1c4b893985cc0ccaff4
Max <enrique.guido@yahoo.com>
2017-05-09 15:09:54 -0400

    Created new route and added projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -61,6 +61,10 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def render_project
+    @projects = Project.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
