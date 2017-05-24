3757bb29aa6b541dc31dc79aa1203faf0a0bb0cc
Max <enrique.guido@yahoo.com>
2017-05-24 00:05:47 -0400

    Created project1_description HTML and correcsponding CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'projects#project_home'
   get '/project1_description' => 'projects#project1_description'
   get '/project2_description' => 'projects#project2_description'
+  get '/project3_description' => 'projects#project3_description'
   # get '/project_description' => 'projects#project3_description'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
